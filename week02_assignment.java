b9646de1ff7a9c44ba7bfaf39f368db94a4765bf
kgowtham007 <gk9177@srmist.edu.in>
2026-03-03 02:23:44 +0530

    Assignment 1 of week 2

---
+++ week02_assignment.java
@@ -0,0 +1,15 @@
+// A program to check if divisible by 5
+import java.util.Scanner;
+public class week02_assignment
+{
+  public static void main(String args[])
+  {
+    Scanner input = new Scanner(System.in);
+	System.out.println("Enter your number:");
+	int number = input.nextInt();
+	if (number /5 ==0 )
+	System.out.println("Is the number " + number + " divisible by 5? YES!");
+	else
+	System.out.println("Is the number " + number + " divisible by 5? YES!");
+  }
+}
